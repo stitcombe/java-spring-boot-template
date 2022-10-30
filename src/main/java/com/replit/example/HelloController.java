0b4d8eb8774b015fe2e79a879fb0ae4acc0a024e
Stephen Titcombe <40187876+stitcombe@users.noreply.github.com>
2022-10-30 21:41:38 +0000

    Updated build.gradle

---
+++ src/main/java/com/replit/example/HelloController.java
@@ -0,0 +1,14 @@
+package com.replit.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
